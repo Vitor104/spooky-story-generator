343f7e3a128dd4751b2036f3fee2e0f373d4f51e
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-10-15 13:38:53 -0300

    fiz algumas mudanças no header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Castle from './sprite5-sheet0.png';
+import Castle from './Castle 1024px.png';
 import styles from './header.module.css';
 import Bat from './bat1.png';
 import NavBar from "../navbar/navBar";
@@ -7,12 +7,7 @@ import NavBar from "../navbar/navBar";
 function Header() {
     return (
     <div className={styles.header}>
-        <div>
-            <img className={styles.castle} src={Castle} alt="castle"></img>
-        </div>
-        <h1 className={styles.welcome}>Welcome</h1>
-        <NavBar />
-        <img className={styles.animatedBat} src={Bat} alt="bat"></img>
+        <img className={styles.castle} src={Castle} alt="castle"></img>
     </div>
     )
 }
