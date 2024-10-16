2bcc0659bc51e3d966b63063305aeefc3cc3e984
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-10-16 20:27:29 -0300

    o header está quase finalizado

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Castle from './Castle 1024px.png';
+import hidePumpkin from './pumpkin1.png';
+import pumpkin from './pumpkin-2892303_1280.jpg';
 import styles from './header.module.css';
 import Bat from './bat1.png';
 import NavBar from "../navbar/navBar";
@@ -7,7 +8,13 @@ import NavBar from "../navbar/navBar";
 function Header() {
     return (
     <div className={styles.header}>
-        <img className={styles.castle} src={Castle} alt="castle"></img>
+        <section className={styles.appName}>
+            <img className={styles.pumpkinImage} src={pumpkin} alt="hide pumpkin"></img>
+            <h1 className={styles.name}>Spooky Story Generator</h1>
+        </section>
+        <NavBar />
+        <h2 class='text-light'>Welcome!</h2>
+        <button type="button" class="btn btn-danger btn-lg">Create a new story</button>
     </div>
     )
 }
--- src/components/navbar/navBar.js
+++ src/components/navbar/navBar.js
@@ -4,18 +4,28 @@ import Pumpkin1 from '../header/pumpkin1.png';
 
 function NavBar() {
     return (
-        <nav className={styles.nav}>
-            <ul className={styles.ul}>
-                <li className={styles.li}>
-                    <img className={styles.hoverPumpkin} src={Pumpkin1} alt="Pumpkin"></img>
-                    About me
-                </li>
-                <li>
-                    <img className={styles.hoverPumpkin} src={Pumpkin1} alt="Pumpkin"></img>
-                    Home
-                </li>
-            </ul>
-        </nav>
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark rounded p-3">
+        <a class="navbar-brand text-white" href="#">SSG</a>
+        <button class="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link text-white" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#">About me</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled text-white" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
     )
 }
 
