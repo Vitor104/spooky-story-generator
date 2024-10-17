637bc4bb03d84cbc72f69659a2d70db65e542206
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-10-17 18:27:38 -0300

    projeto está quase finalizado

---
+++ src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './footer.module.css';
+
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h5>2024 - Happy Halloween!</h5>
+        </footer>
+    )
+}
+
+
+export default Footer;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,7 +3,7 @@ import hidePumpkin from './pumpkin1.png';
 import pumpkin from './pumpkin-2892303_1280.jpg';
 import styles from './header.module.css';
 import Bat from './bat1.png';
-import NavBar from "../navbar/navBar";
+import Footer from "../footer/footer";
 
 function Header() {
     return (
@@ -12,9 +12,14 @@ function Header() {
             <img className={styles.pumpkinImage} src={pumpkin} alt="hide pumpkin"></img>
             <h1 className={styles.name}>Spooky Story Generator</h1>
         </section>
-        <NavBar />
-        <h2 class='text-light'>Welcome!</h2>
-        <button type="button" class="btn btn-danger btn-lg">Create a new story</button>
+        <div className={styles.divBtn}>
+            <h2>Welcome to the <span>Spooky Story Generator!</span></h2>
+            <p>If you're ready, click the button and generate a spooky story!</p>
+            <button type="button" class="btn btn-warning btn-lg">Create a new story</button>
+        </div>
+        <div></div>
+        
+        
     </div>
     )
 }
