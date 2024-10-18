6f9579f92802e01bcb76ed3e30645623fe33640a
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-10-18 20:27:56 -0300

    pequenas alterações

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
+import StoryCard from './components/storyCard/storyCard';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <StoryCard />
       <Footer />
+
     </div>
   );
 }
